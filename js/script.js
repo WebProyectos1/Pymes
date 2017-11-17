cc1e354ee4e1d5e4bdd379c79bd6b262f81412ce
arturo <arturoo_rc@outlook.com>
2017-11-16 18:22:12 -0600

    Barra de navegacion y funcionalidad de las anclas

---
+++ js/script.js
@@ -0,0 +1,42 @@
+ 
+
+
+
+
+
+ $(document).ready(function(){
+
+		 //nos desplazamos entre todos los divs
+		 $('a.ancla').click(function(e){
+		 	
+		 	e.preventDefault();
+
+		    enlace  = $(this).attr('href');
+
+		    $('html, body').animate({
+		    	scrollTop: $(enlace).offset().top
+		    }, 1000);
+
+		 });
+
+
+
+
+		 /*//vamos al principio o al final de la página
+		 $('a.arriba').click(function(e){
+		 e.preventDefault();
+		     volver  = $(this).attr('href');
+		     $('html, body').animate({
+		         scrollTop: $(volver).offset().top
+		     }, 2000);
+		 });
+
+
+
+
+
+		 //pasando la cantidad de pixeles que queremos a scrollTop
+		 $('.prueba').click(function(){
+		 $('html, body').animate({scrollTop:100}, 2000); return false;
+		 });*/
+ });
