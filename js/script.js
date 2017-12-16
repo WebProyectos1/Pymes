c3da61d961064052e74d67128e4e41fd80e63239
arturo <arturoo_rc@outlook.com>
2017-12-16 17:20:10 -0600

    Se agrego el efecto del scroll (pluging jquery.onepage-scroll.js)

--- js/script.js
+++ js/script.js
@@ -5,38 +5,101 @@
 
 
  $(document).ready(function(){
-
-		 //nos desplazamos entre todos los divs
-		 $('a.ancla').click(function(e){
+ 		 $(".main").onepage_scroll();
+		 
+		 $('a.ancla, a.ancla2').click(function(e){
 		 	
+
 		 	e.preventDefault();
 
 		    enlace  = $(this).attr('href');
+		    identificador = $(this).attr('id');
 
-		    $('html, body').animate({
-		    	scrollTop: $(enlace).offset().top - 50
+		    desplazamiento(enlace);
+		    cambiarClase(identificador);
+
+		 });
+
+	
+
+		
+
+	
+
+ });
+
+
+/*function detectarScroll(){
+			
+
+       		var secciones =["seccion1","seccion2","seccion3"];	
+             
+    		var obj = $(document);         
+    		var obj_top = obj.scrollTop()   
+
+    		obj.scroll(function(){
+
+        		var obj_act_top = $(this).scrollTop();  
+
+       			if(obj_act_top > obj_top){
+            		
+            	var prueba = 1;
+
+            	prueba = prueba + 1
+
+            	console.log(prueba);
+            
+        		}else{
+            	
+            	console.log("Arriba");
+            
+       			}
+        		obj_top = obj_act_top;                
+    		});
+		
+
+
+		}*/
+
+			function desplazamiento(posicion){
+
+			 $('html, body').animate({
+		    	scrollTop: $(posicion).offset().top - 50
 		    }, 1000);
 
-		 });
+
+		}
+		
+
+		
+			
 
 
+		/*function cambiarClase(par_id,){
+			
+			switch(par_id) {
+			    case 'btn_sup1':
+			    	$('#btn_sup1').
+			        break;
+			    case 'btn_sup2':
+			        code block
+			        break;
+			    case 'btn_sup3':
+			        code block
+			        break;
+			    case 'btn_lat1':
+			        code block
+			        break;
+			    case 'btn_lat2':
+			        code block
+			        break;
+			    case 'btn_lat3':
+			        code block
+			        break;
+			    default:
+			        code block
+			}
+			
+		}*/
 
-
-		 /*//vamos al principio o al final de la página
-		 $('a.arriba').click(function(e){
-		 e.preventDefault();
-		     volver  = $(this).attr('href');
-		     $('html, body').animate({
-		         scrollTop: $(volver).offset().top
-		     }, 2000);
-		 });
-
-
-
-
-
-		 //pasando la cantidad de pixeles que queremos a scrollTop
-		 $('.prueba').click(function(){
-		 $('html, body').animate({scrollTop:100}, 2000); return false;
-		 });*/
- });
+		
